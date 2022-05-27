f703518a3c1b6274fd241e8c1d276823ffee83ac
xweb3 <xquotes-web3@protonmail.com>
2022-05-27 10:43:34 +0800

    feat: 🎸 add useTokenByDefault
    add useTokenByDefault

--- src/hooks/useToken/index.ts
+++ src/hooks/useToken/index.ts
@@ -15,6 +15,11 @@ export const useTokenBySymbol = (symbol: string) => {
   return data.find((v: ITokenInfo) => v.symbol === symbol);
 }
 
+export const useTokenByDefault = (filter: Record<keyof ITokenInfo, any>) => {
+  const { data = [] } = useFetcher(tokensApi);
+  return data.filter((val: ITokenInfo) => Object.keys(filter).map(v => filter[v] === val[v]).every(v => v))[0];
+} 
+
 export const useToken = (filter: Record<keyof ITokenInfo, any>) => {
   const { data = [] } = useFetcher(tokensApi);
   return data.find((val: ITokenInfo) => Object.keys(filter).map(v => filter[v] === val[v]).every(v => v));
